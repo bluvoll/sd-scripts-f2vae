0efb2a2eaf0fd2af5c3c86b7e9a17dd7d92322f2
bluvoll <gordigo24@hotmail.com>
2026-01-11 18:37:02 -0500

    gitignore changes

--- launch_all_train.sh
+++ launch_all_train.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-accelerate launch sdxl_train_network.py --dataset_config="dataset1.toml" --config_file="config1.toml"  --flow_model  --flow_use_ot  --flow_timestep_distribution logit_normal --flow_uniform_static_ratio 2.5 --vae_batch_size 1 --flow_logit_mean -0.2  --flow_logit_std 1.5  --use_zero_cond=True --vae_type flux2 --latent_channels 32 --vae_custom_scale 0.6043 --vae_custom_shift 0.0760 --ddp_timeout 3600 --network_train_unet_only  --disable_cross_attn_mask
-echo "All training jobs finished. Press any key to close..."
-read -n 1 -s -r -p ""
+accelerate launch sdxl_train_network.py --dataset_config="dataset1.toml" --config_file="config1.toml"  --flow_model  --flow_use_ot  --flow_timestep_distribution logit_normal --flow_uniform_static_ratio 3 --vae_batch_size 6 --flow_logit_mean -0.2  --flow_logit_std 1.5 --use_zero_cond=False --ddp_timeout 3600 --network_train_unet_only
